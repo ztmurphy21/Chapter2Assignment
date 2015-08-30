238f9bf7b72842b6ff6b9ae2e4d17f765bd9a191
Zachary Murphy <murphyz@my.wccc.edu>
2015-08-30 16:13:17 -0400

    Code for Ch2Assignment

---
+++ Ch2Assignment.java
@@ -0,0 +1,33 @@
+
+package ch2assignment;
+
+/**
+ *
+ * @author Zachary
+ */
+public class Ch2Assignment {
+
+  
+    public static void main(String[] args) {
+       char letterOne='p';
+       char letterTwo='e';
+       char letterThree='r';
+       char letterFour='f';
+       char letterFive='e';
+       char letterSix='c';
+       char letterSeven ='t';
+       double asciiSum;
+       
+       System.out.println("The word is 'perfect'.");
+       System.out.print("The sum of the word perfect is  ");
+       
+       asciiSum= (int)letterOne + (int)letterTwo + (int)letterThree + (int)letterFour + (int)letterFive + (int)letterSix + (int)letterSeven; 
+       
+       System.out.print(asciiSum);
+       System.out.print(".");
+       
+       
+               
+    }
+    
+}
